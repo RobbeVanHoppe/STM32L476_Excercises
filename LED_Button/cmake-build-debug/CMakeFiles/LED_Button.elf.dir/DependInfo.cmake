24fa52f532fb8858c8452d2e69c47d05a2d9cb49
robbevh <robbeavnhoppe@outlook.com>
2023-10-25 17:37:38 +0200

    first commit

---
+++ LED_Button/cmake-build-debug/CMakeFiles/LED_Button.elf.dir/DependInfo.cmake
@@ -0,0 +1,64 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "ASM"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_ASM
+  "/home/robbevh/CLionProjects/STM32/LED_Button/Core/Startup/startup_stm32l476rgtx.s" "/home/robbevh/CLionProjects/STM32/LED_Button/cmake-build-debug/CMakeFiles/LED_Button.elf.dir/Core/Startup/startup_stm32l476rgtx.s.obj"
+  )
+set(CMAKE_ASM_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_ASM
+  "DEBUG"
+  "STM32L476xx"
+  "USE_HAL_DRIVER"
+  )
+
+# The include file search paths:
+set(CMAKE_ASM_TARGET_INCLUDE_PATH
+  "/home/robbevh/CLionProjects/STM32/LED_Button/Core/Inc"
+  "/home/robbevh/CLionProjects/STM32/LED_Button/Drivers/STM32L4xx_HAL_Driver/Inc"
+  "/home/robbevh/CLionProjects/STM32/LED_Button/Drivers/STM32L4xx_HAL_Driver/Inc/Legacy"
+  "/home/robbevh/CLionProjects/STM32/LED_Button/Drivers/CMSIS/Device/ST/STM32L4xx/Include"
+  "/home/robbevh/CLionProjects/STM32/LED_Button/Drivers/CMSIS/Include"
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/home/robbevh/CLionProjects/STM32/LED_Button/Core/Src/excercise2.c" "CMakeFiles/LED_Button.elf.dir/Core/Src/excercise2.c.obj" "gcc" "CMakeFiles/LED_Button.elf.dir/Core/Src/excercise2.c.obj.d"
+  "/home/robbevh/CLionProjects/STM32/LED_Button/Core/Src/main.c" "CMakeFiles/LED_Button.elf.dir/Core/Src/main.c.obj" "gcc" "CMakeFiles/LED_Button.elf.dir/Core/Src/main.c.obj.d"
+  "/home/robbevh/CLionProjects/STM32/LED_Button/Core/Src/stm32l4xx_hal_msp.c" "CMakeFiles/LED_Button.elf.dir/Core/Src/stm32l4xx_hal_msp.c.obj" "gcc" "CMakeFiles/LED_Button.elf.dir/Core/Src/stm32l4xx_hal_msp.c.obj.d"
+  "/home/robbevh/CLionProjects/STM32/LED_Button/Core/Src/stm32l4xx_it.c" "CMakeFiles/LED_Button.elf.dir/Core/Src/stm32l4xx_it.c.obj" "gcc" "CMakeFiles/LED_Button.elf.dir/Core/Src/stm32l4xx_it.c.obj.d"
+  "/home/robbevh/CLionProjects/STM32/LED_Button/Core/Src/syscalls.c" "CMakeFiles/LED_Button.elf.dir/Core/Src/syscalls.c.obj" "gcc" "CMakeFiles/LED_Button.elf.dir/Core/Src/syscalls.c.obj.d"
+  "/home/robbevh/CLionProjects/STM32/LED_Button/Core/Src/sysmem.c" "CMakeFiles/LED_Button.elf.dir/Core/Src/sysmem.c.obj" "gcc" "CMakeFiles/LED_Button.elf.dir/Core/Src/sysmem.c.obj.d"
+  "/home/robbevh/CLionProjects/STM32/LED_Button/Core/Src/system_stm32l4xx.c" "CMakeFiles/LED_Button.elf.dir/Core/Src/system_stm32l4xx.c.obj" "gcc" "CMakeFiles/LED_Button.elf.dir/Core/Src/system_stm32l4xx.c.obj.d"
+  "/home/robbevh/CLionProjects/STM32/LED_Button/Drivers/STM32L4xx_HAL_Driver/Src/stm32l4xx_hal.c" "CMakeFiles/LED_Button.elf.dir/Drivers/STM32L4xx_HAL_Driver/Src/stm32l4xx_hal.c.obj" "gcc" "CMakeFiles/LED_Button.elf.dir/Drivers/STM32L4xx_HAL_Driver/Src/stm32l4xx_hal.c.obj.d"
+  "/home/robbevh/CLionProjects/STM32/LED_Button/Drivers/STM32L4xx_HAL_Driver/Src/stm32l4xx_hal_cortex.c" "CMakeFiles/LED_Button.elf.dir/Drivers/STM32L4xx_HAL_Driver/Src/stm32l4xx_hal_cortex.c.obj" "gcc" "CMakeFiles/LED_Button.elf.dir/Drivers/STM32L4xx_HAL_Driver/Src/stm32l4xx_hal_cortex.c.obj.d"
+  "/home/robbevh/CLionProjects/STM32/LED_Button/Drivers/STM32L4xx_HAL_Driver/Src/stm32l4xx_hal_dma.c" "CMakeFiles/LED_Button.elf.dir/Drivers/STM32L4xx_HAL_Driver/Src/stm32l4xx_hal_dma.c.obj" "gcc" "CMakeFiles/LED_Button.elf.dir/Drivers/STM32L4xx_HAL_Driver/Src/stm32l4xx_hal_dma.c.obj.d"
+  "/home/robbevh/CLionProjects/STM32/LED_Button/Drivers/STM32L4xx_HAL_Driver/Src/stm32l4xx_hal_dma_ex.c" "CMakeFiles/LED_Button.elf.dir/Drivers/STM32L4xx_HAL_Driver/Src/stm32l4xx_hal_dma_ex.c.obj" "gcc" "CMakeFiles/LED_Button.elf.dir/Drivers/STM32L4xx_HAL_Driver/Src/stm32l4xx_hal_dma_ex.c.obj.d"
+  "/home/robbevh/CLionProjects/STM32/LED_Button/Drivers/STM32L4xx_HAL_Driver/Src/stm32l4xx_hal_exti.c" "CMakeFiles/LED_Button.elf.dir/Drivers/STM32L4xx_HAL_Driver/Src/stm32l4xx_hal_exti.c.obj" "gcc" "CMakeFiles/LED_Button.elf.dir/Drivers/STM32L4xx_HAL_Driver/Src/stm32l4xx_hal_exti.c.obj.d"
+  "/home/robbevh/CLionProjects/STM32/LED_Button/Drivers/STM32L4xx_HAL_Driver/Src/stm32l4xx_hal_flash.c" "CMakeFiles/LED_Button.elf.dir/Drivers/STM32L4xx_HAL_Driver/Src/stm32l4xx_hal_flash.c.obj" "gcc" "CMakeFiles/LED_Button.elf.dir/Drivers/STM32L4xx_HAL_Driver/Src/stm32l4xx_hal_flash.c.obj.d"
+  "/home/robbevh/CLionProjects/STM32/LED_Button/Drivers/STM32L4xx_HAL_Driver/Src/stm32l4xx_hal_flash_ex.c" "CMakeFiles/LED_Button.elf.dir/Drivers/STM32L4xx_HAL_Driver/Src/stm32l4xx_hal_flash_ex.c.obj" "gcc" "CMakeFiles/LED_Button.elf.dir/Drivers/STM32L4xx_HAL_Driver/Src/stm32l4xx_hal_flash_ex.c.obj.d"
+  "/home/robbevh/CLionProjects/STM32/LED_Button/Drivers/STM32L4xx_HAL_Driver/Src/stm32l4xx_hal_flash_ramfunc.c" "CMakeFiles/LED_Button.elf.dir/Drivers/STM32L4xx_HAL_Driver/Src/stm32l4xx_hal_flash_ramfunc.c.obj" "gcc" "CMakeFiles/LED_Button.elf.dir/Drivers/STM32L4xx_HAL_Driver/Src/stm32l4xx_hal_flash_ramfunc.c.obj.d"
+  "/home/robbevh/CLionProjects/STM32/LED_Button/Drivers/STM32L4xx_HAL_Driver/Src/stm32l4xx_hal_gpio.c" "CMakeFiles/LED_Button.elf.dir/Drivers/STM32L4xx_HAL_Driver/Src/stm32l4xx_hal_gpio.c.obj" "gcc" "CMakeFiles/LED_Button.elf.dir/Drivers/STM32L4xx_HAL_Driver/Src/stm32l4xx_hal_gpio.c.obj.d"
+  "/home/robbevh/CLionProjects/STM32/LED_Button/Drivers/STM32L4xx_HAL_Driver/Src/stm32l4xx_hal_pwr.c" "CMakeFiles/LED_Button.elf.dir/Drivers/STM32L4xx_HAL_Driver/Src/stm32l4xx_hal_pwr.c.obj" "gcc" "CMakeFiles/LED_Button.elf.dir/Drivers/STM32L4xx_HAL_Driver/Src/stm32l4xx_hal_pwr.c.obj.d"
+  "/home/robbevh/CLionProjects/STM32/LED_Button/Drivers/STM32L4xx_HAL_Driver/Src/stm32l4xx_hal_pwr_ex.c" "CMakeFiles/LED_Button.elf.dir/Drivers/STM32L4xx_HAL_Driver/Src/stm32l4xx_hal_pwr_ex.c.obj" "gcc" "CMakeFiles/LED_Button.elf.dir/Drivers/STM32L4xx_HAL_Driver/Src/stm32l4xx_hal_pwr_ex.c.obj.d"
+  "/home/robbevh/CLionProjects/STM32/LED_Button/Drivers/STM32L4xx_HAL_Driver/Src/stm32l4xx_hal_rcc.c" "CMakeFiles/LED_Button.elf.dir/Drivers/STM32L4xx_HAL_Driver/Src/stm32l4xx_hal_rcc.c.obj" "gcc" "CMakeFiles/LED_Button.elf.dir/Drivers/STM32L4xx_HAL_Driver/Src/stm32l4xx_hal_rcc.c.obj.d"
+  "/home/robbevh/CLionProjects/STM32/LED_Button/Drivers/STM32L4xx_HAL_Driver/Src/stm32l4xx_hal_rcc_ex.c" "CMakeFiles/LED_Button.elf.dir/Drivers/STM32L4xx_HAL_Driver/Src/stm32l4xx_hal_rcc_ex.c.obj" "gcc" "CMakeFiles/LED_Button.elf.dir/Drivers/STM32L4xx_HAL_Driver/Src/stm32l4xx_hal_rcc_ex.c.obj.d"
+  "/home/robbevh/CLionProjects/STM32/LED_Button/Drivers/STM32L4xx_HAL_Driver/Src/stm32l4xx_hal_tim.c" "CMakeFiles/LED_Button.elf.dir/Drivers/STM32L4xx_HAL_Driver/Src/stm32l4xx_hal_tim.c.obj" "gcc" "CMakeFiles/LED_Button.elf.dir/Drivers/STM32L4xx_HAL_Driver/Src/stm32l4xx_hal_tim.c.obj.d"
+  "/home/robbevh/CLionProjects/STM32/LED_Button/Drivers/STM32L4xx_HAL_Driver/Src/stm32l4xx_hal_tim_ex.c" "CMakeFiles/LED_Button.elf.dir/Drivers/STM32L4xx_HAL_Driver/Src/stm32l4xx_hal_tim_ex.c.obj" "gcc" "CMakeFiles/LED_Button.elf.dir/Drivers/STM32L4xx_HAL_Driver/Src/stm32l4xx_hal_tim_ex.c.obj.d"
+  "/home/robbevh/CLionProjects/STM32/LED_Button/Drivers/STM32L4xx_HAL_Driver/Src/stm32l4xx_hal_uart.c" "CMakeFiles/LED_Button.elf.dir/Drivers/STM32L4xx_HAL_Driver/Src/stm32l4xx_hal_uart.c.obj" "gcc" "CMakeFiles/LED_Button.elf.dir/Drivers/STM32L4xx_HAL_Driver/Src/stm32l4xx_hal_uart.c.obj.d"
+  "/home/robbevh/CLionProjects/STM32/LED_Button/Drivers/STM32L4xx_HAL_Driver/Src/stm32l4xx_hal_uart_ex.c" "CMakeFiles/LED_Button.elf.dir/Drivers/STM32L4xx_HAL_Driver/Src/stm32l4xx_hal_uart_ex.c.obj" "gcc" "CMakeFiles/LED_Button.elf.dir/Drivers/STM32L4xx_HAL_Driver/Src/stm32l4xx_hal_uart_ex.c.obj.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
